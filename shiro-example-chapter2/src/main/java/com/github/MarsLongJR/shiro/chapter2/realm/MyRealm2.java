657a7919c4b23fc2d4440b372ffffc6b7e768d93
Mars <13122522723@163.com>
2019-03-14 13:46:24 +0800

    简单的登录测试 单个realm测试 多个realm测试

--- shiro-example-chapter2/src/main/java/com/github/MarsLongJR/shiro/chapter2/realm/MyRealm2.java
+++ shiro-example-chapter2/src/main/java/com/github/MarsLongJR/shiro/chapter2/realm/MyRealm2.java
@@ -1,10 +1,33 @@
 package com.github.MarsLongJR.shiro.chapter2.realm;
 
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
 /**
  * @ClassName
  * @Description
  * @company:www.xinbeize.com
  * @author:Mars
  */
-public class MyRealm2 {
+public class MyRealm2 implements Realm {
+    public String getName() {
+        return "MyRealm2";
+    }
+
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+            String name= (String) token.getPrincipal();
+            String password=new String((char [])token.getCredentials());
+            if(!"wang".equals(name)){
+                throw new UnknownAccountException();
+            }
+            if(!"123".equals(password)){
+                throw new IncorrectCredentialsException();
+            }
+            //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+            return new SimpleAuthenticationInfo(name,password,this.getName());
+    }
 }
